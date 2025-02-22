fbedbe962850e2757bf336505e0f9aed048fb613
Yi-Xuan Lau <yixuan.lau@gmail.com>
2025-02-22 14:48:51 +0800

    Exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,8 +1,8 @@
 import { useState, useEffect } from "react";
 import Persons from "./components/Persons";
-import PersonForm from './components/PersonForm';
-import Filter from './components/Filter';
-import axios from 'axios';
+import PersonForm from "./components/PersonForm";
+import Filter from "./components/Filter";
+import axios from "axios";
 
 const App = () => {
   const [persons, setPersons] = useState([]);
@@ -10,32 +10,30 @@ const App = () => {
   const [newNumber, setNewNumber] = useState("");
   const [filter, setNewFilter] = useState("");
 
-  useEffect( () => {
-    axios
-    .get('http://localhost:3001/persons')
-    .then(response => {
-      setPersons(response.data)
-  })
-}, []);
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data);
+    });
+  }, []);
 
   const handleFilterChange = (e) => {
-    setNewFilter(e.target.value)
-  }
+    setNewFilter(e.target.value);
+  };
 
-    const handleNumberChange = (e) => {
-      setNewNumber(e.target.value);
-    };
+  const handleNumberChange = (e) => {
+    setNewNumber(e.target.value);
+  };
 
-    const handleNameChange = (e) => {
-      setNewName(e.target.value);
-    };
+  const handleNameChange = (e) => {
+    setNewName(e.target.value);
+  };
 
   const addEntry = (e) => {
     e.preventDefault();
     const newPerson = {
       name: newName,
       number: newNumber,
-      id: persons.length + 1
+      id: persons.length + 1,
     };
 
     if (persons.some((person) => person.name === newName)) {
@@ -50,7 +48,11 @@ const App = () => {
       <h2>Phonebook</h2>
       <Filter onChange={handleFilterChange} value={filter} />
       <h2>add a new</h2>
-      <PersonForm onSubmit={addEntry} handleNumberChange={handleNumberChange} handleNameChange={handleNameChange} />
+      <PersonForm
+        onSubmit={addEntry}
+        handleNumberChange={handleNumberChange}
+        handleNameChange={handleNameChange}
+      />
       <h2>Numbers</h2>
       <Persons entries={persons} filter={filter} />
     </div>
