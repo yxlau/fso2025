e1de2d5d20fc03ccf51025743110a115853018fe
Yi-Xuan Lau <yixuan.lau@clls.com>
2025-03-28 17:37:05 +0800

    Exercise 4.20

--- part4/bloglist/controllers/login.js
+++ part4/bloglist/controllers/login.js
@@ -21,6 +21,8 @@ loginRouter.post("/", async (request, response) => {
     id: user._id,
   };
 
+  const token = jwt.sign(userForToken, process.env.SECRET);
+
   response
     .status(200)
     .send({ token, username: user.username, name: user.name });
