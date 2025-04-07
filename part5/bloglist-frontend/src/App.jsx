557e2fc6479e366f53c54be68645e59da8766bc1
Yi-Xuan Lau <yixuan.lau@clls.com>
2025-04-07 16:09:49 +0800

    Exercise 5.2

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -1,24 +1,84 @@
-import { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
+import { useState, useEffect } from "react";
+import Blog from "./components/Blog";
+import blogService from "./services/blogs";
+import loginService from "./services/login";
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+  const [blogs, setBlogs] = useState([]);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState("");
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+    blogService.getAll().then((blogs) => setBlogs(blogs));
+  }, []);
 
-  return (
-    <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
-    </div>
-  )
-}
+  const loginHandler = async (e) => {
+    e.preventDefault();
 
-export default App
+    try {
+      const response = await loginService.login({ username, password });
+      setUser(response);
+      window.localStorage.setItem("token", response.token);
+    } catch (error) {
+      console.error("Error:", error.message);
+    }
+    setUsername("");
+    setPassword("");
+  };
+
+  const loginForm = () => (
+    <form onSubmit={loginHandler}>
+      <div>
+        <label htmlFor="username">username</label>
+        <input
+          type="text"
+          name="username"
+          onChange={({ target }) => {
+            setUsername(target.value);
+          }}
+          value={username}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">password</label>
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  );
+
+  const handleLogout = () => {
+    window.localStorage.removeItem("token");
+    setUser("");
+  };
+
+  if (user) {
+    return (
+      <div>
+        <h2>blogs</h2>
+        <p>
+          {user ? `${user.name} logged in` : ""}
+          <button onClick={handleLogout}>Logout</button>
+        </p>
+        {blogs.map((blog) => (
+          <Blog key={blog.id} blog={blog} />
+        ))}
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h2>Log in to application</h2>
+        {loginForm()}
+      </div>
+    );
+  }
+};
+
+export default App;
