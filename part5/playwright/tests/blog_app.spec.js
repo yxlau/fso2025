f01cab712ea382344a8778a4270106be77d1ee0b
Yi-Xuan Lau <yixuan.lau@clls.com>
2025-05-09 17:38:56 +0800

    Exercise 5.22

--- part5/playwright/tests/blog_app.spec.js
+++ part5/playwright/tests/blog_app.spec.js
@@ -14,7 +14,7 @@ describe('Blog app', () => {
   })
 
   describe('Login', () => {
-    beforeEach(async({page, request}) => {
+    beforeEach(async ({ page, request }) => {
       await request.post("http://localhost:3001/api/testing/reset")
       await request.post("http://localhost:3001/api/users", {
         data: {
@@ -25,7 +25,7 @@ describe('Blog app', () => {
       })
     })
     test('succeeds with correct credentials', async ({ page }) => {
-     await loginWith(page, 'testy', 'testword')
+      await loginWith(page, 'testy', 'testword')
 
       await expect(page.getByText("tester logged in")).toBeVisible()
     })
@@ -38,44 +38,92 @@ describe('Blog app', () => {
 
     })
 
+    test('cannot delete other blogs', async ({ page, request }) => {
+      const userResponse = await request.post("http://localhost:3001/api/users", {
+        data: {
+          name: 'othertester',
+          password: 'otherword',
+          username: 'other'
+        }
+      })
+
+      const otherUser = userResponse.json()
+
+      const loginResponse = await request.post("http://localhost:3001/api/login", {
+        data: {
+          username: 'other',
+          password: 'otherword',
+        }
+      });
+
+      const loginData = await loginResponse.json();
+      const token = loginData.token;
+
+      // Create blog with authorization header
+      const blogResponse = await request.post("http://localhost:3001/api/blogs", {
+        data: {
+          title: "other title",
+          author: "other author",
+          url: "http://otherurl.com",
+        },
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+      });
+
+      await page.waitForTimeout(3000)
+
+      await loginWith(page, 'testy', 'testword')
+      await expect(page.getByText("tester logged in")).toBeVisible()
+
+      await page.getByText('other title, other author').waitFor()
+      await page.getByRole('button', { name: 'view' }).last().click()
+      await expect(page.getByRole('button', { name: 'remove' })).not.toBeVisible()
+    })
+
     describe('When logged in', () => {
-      beforeEach(async({page}) => {
+      beforeEach(async ({ page }) => {
         await loginWith(page, 'testy', 'testword')
       })
 
-      test('can create blog', async({page}) => {
+      test('can create blog', async ({ page }) => {
         await createBlog(page, 'the title', 'the author', 'http://url.com')
         await page.getByText('the title, the author').waitFor()
 
         await expect(page.getByText("the title, the author")).toBeVisible()
       })
 
-      test('can like a blog', async({page}) => {
-        await page.getByRole('button', {name: 'view'}).waitFor()
-        await page.getByRole('button', {name: 'view'}).first().click()
+      test('can like a blog', async ({ page }) => {
+        await page.getByRole('button', { name: 'view' }).waitFor()
+        await page.getByRole('button', { name: 'view' }).first().click()
         const likeCount = parseInt(await page.getByTestId('likeCount').first().innerText())
-        await page.getByRole('button', {name: 'like'}).first().click()
+        await page.getByRole('button', { name: 'like' }).first().click()
         await page.waitForTimeout(3000)
         const newCount = parseInt(await page.getByTestId('likeCount').first().innerText())
 
         await expect(newCount).toBe(likeCount + 1)
       })
 
-      test('can delete own blog', async({page}) => {
+      test('can delete own blog', async ({ page }) => {
         const title = `removable ${Math.floor(Math.random() * 1001)}`
-        await createBlog(page, title , 'the author', 'http://removable.com')
+        await createBlog(page, title, 'the author', 'http://removable.com')
         await page.getByText(`${title}, the author`).waitFor()
-        await page.getByRole('button', {name: 'view'}).last().click()
+        await page.getByRole('button', { name: 'view' }).last().click()
         page.on('dialog', dialog => dialog.accept());
-        await page.getByRole('button', {name: 'remove'} ).click()
+        await page.getByRole('button', { name: 'remove' }).click()
 
         await expect(page.getByText(`${title}, the author`)).not.toBeVisible()
       })
 
-      
-    })
+
+
     })
 
-   
+
+
   })
+
+
+
+})
 
